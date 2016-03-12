590312b553661fdf8199d70ab747c70b4e8e89b4
Michael <Michael@DESKTOP-6C5L4LF>
2016-03-12 23:15:44 +0800

    First Commit

---
+++ src/sg/edu/nus/iss/universitysouvenirstore/StoreApplication.java
@@ -0,0 +1,11 @@
+package sg.edu.nus.iss.universitysouvenirstore;
+
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("University Souvenir Store Application!");
+
+	}
+
+}
