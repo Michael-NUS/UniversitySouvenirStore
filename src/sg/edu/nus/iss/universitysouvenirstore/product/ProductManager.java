c9827421ada44093713cad9ef920abd960a462a6
aslin1126 <63052533@qq.com>
2016-03-16 20:46:08 +0800

    update product

---
+++ src/sg/edu/nus/iss/universitysouvenirstore/product/ProductManager.java
@@ -0,0 +1,5 @@
+package sg.edu.nus.iss.universitysouvenirstore.product;
+
+public class ProductManager {
+
+}
---
+++ test/sg/edu/nus/iss/universitysouvenirstore/product/ProductManagerTest.java
@@ -0,0 +1,14 @@
+package sg.edu.nus.iss.universitysouvenirstore.product;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProductManagerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
