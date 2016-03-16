b0d9e0b9530a5d76a0b53aa25cf2a01407f02a00
aslin1126 <63052533@qq.com>
2016-03-16 21:11:00 +0800

    update product

--- src/sg/edu/nus/iss/universitysouvenirstore/product/Product.java
+++ src/sg/edu/nus/iss/universitysouvenirstore/product/Product.java
@@ -1,5 +1,75 @@
 package sg.edu.nus.iss.universitysouvenirstore.product;
 
 public class Product {
+	private String productId;
+	private String productName;
+	private String briefDescription;
+	private int availableQuantity;
+	private double price;
+	private String barCodeNumber;
+	private int reorderQuantity;
+	private int orderQuantity;
+	private String categoryId;
+	
+	
+	
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getBriefDescription() {
+		return briefDescription;
+	}
+	public void setBriefDescription(String briefDescription) {
+		this.briefDescription = briefDescription;
+	}
+	public int getAvailableQuantity() {
+		return availableQuantity;
+	}
+	public void setAvailableQuantity(int availableQuantity) {
+		this.availableQuantity = availableQuantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getBarCodeNumber() {
+		return barCodeNumber;
+	}
+	public void setBarCodeNumber(String barCodeNumber) {
+		this.barCodeNumber = barCodeNumber;
+	}
+	public int getReorderQuantity() {
+		return reorderQuantity;
+	}
+	public void setReorderQuantity(int reorderQuantity) {
+		this.reorderQuantity = reorderQuantity;
+	}
+	public int getOrderQuantity() {
+		return orderQuantity;
+	}
+	public void setOrderQuantity(int orderQuantity) {
+		this.orderQuantity = orderQuantity;
+	}
+	public void updateProduct(){
+		
+	}
 
 }
+
