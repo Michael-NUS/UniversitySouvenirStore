840cf2e9b1170d244ad5561f60f1cd52392edf11
limmi <limmi@LIMMI6.auth.hpicorp.net>
2016-04-05 17:11:54 +0800

    Simple transaction junit tet

--- test/sg/edu/nus/iss/universitysouvenirstore/TransactionTest.java
+++ test/sg/edu/nus/iss/universitysouvenirstore/TransactionTest.java
@@ -12,23 +12,31 @@ public class TransactionTest {
 	
 	private Transaction transaction = new Transaction();
 	private ArrayList<TransactionedItem> item1;
+	
 	@Test
-	public void NewTransaction() {
+	public void NewTransaction() { //tranasction should always starts with $0
 		assertTrue(0 == transaction.GetTotalPrice());
 	}
 	
 	@Test
-	public void AddNewItem(){
+	public void AddNewItem(){ //value should be increased, instead of replaced
 		transaction.AddTransactionItem("CLO/1", 1, 21.45);
-		transaction.IncreaseTransactionItem("CLO/1", 1);
+		transaction.IncreaseTransactionItem("CLO/1", 1); //value should be increased, instead of replaced
 		transaction.IncreaseTransactionItem("CLO/1", 5);
 		item1 = transaction.GetTransactionItems();
 		
-		assertFalse(3 == item1.get(0).GetProductQuantity());
+		assertFalse(5 == item1.get(0).GetProductQuantity());
 	}
 	
-	public void EditItem(){
+	@Test	
+	public void EditItem(){	//edit need to replace the existing value
+		transaction.AddTransactionItem("CLO/1", 1, 21.45);
+		transaction.IncreaseTransactionItem("CLO/1", 1);
+		transaction.IncreaseTransactionItem("CLO/1", 1);
+		transaction.EditTransactionItem("CLO/1", 5); //edit need to replace the existing value
+		item1 = transaction.GetTransactionItems();
 		
-		
+		//System.out.println(String.valueOf(item1.get(0).GetProductQuantity()));			
+		assertTrue(5==item1.get(0).GetProductQuantity());		
 	}
 }
