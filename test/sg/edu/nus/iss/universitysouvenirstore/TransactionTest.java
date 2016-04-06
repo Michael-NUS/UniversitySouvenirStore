6ed5a94ad31abbdf7dbd3e095c6b4634f31756e1
Michael <Michael@DESKTOP-6C5L4LF>
2016-04-06 22:38:25 +0800

    Transaction Unit Testing

--- test/sg/edu/nus/iss/universitysouvenirstore/TransactionTest.java
+++ test/sg/edu/nus/iss/universitysouvenirstore/TransactionTest.java
@@ -7,19 +7,43 @@ import java.util.ArrayList;
 import sg.edu.nus.iss.universitysouvenirstore.*;
 
 import org.junit.Test;
-
+/**
+ * 
+ * @author Michael
+ * Unit test for Transaction Class
+ */
 public class TransactionTest {
 	
 	private Transaction transaction = new Transaction();
 	private ArrayList<TransactionedItem> item1;
 	
+	/**
+	 * Each transaction starts with $0
+	 */
 	@Test
 	public void NewTransaction() { //tranasction should always starts with $0
 		assertTrue(0 == transaction.GetTotalPrice());
 	}
 	
+	/**
+	 * Adding a new Item, 
+	 * expected result: item added properly
+	 * @throws CustomException
+	 */
 	@Test
-	public void AddNewItem() throws CustomException{ //value should be increased, instead of replaced
+	public void AddItem() throws CustomException{ //value should be increased, instead of replaced
+		transaction.AddTransactionItem("CLO/1", 1, 21.45);
+		item1 = transaction.GetTransactionItems();
+		assertTrue(item1.get(0).GetProductID().equals("CLO/1"));
+	}
+	
+	/**
+	 * Increase existing productID's quantity if it's already existing 
+	 * expected result: quantity is increased properly
+	 * @throws CustomException
+	 */
+	@Test
+	public void AddDuplicateItem() throws CustomException{ //value should be increased, instead of replaced
 		transaction.AddTransactionItem("CLO/1", 1, 21.45);
 		transaction.IncreaseTransactionItem("CLO/1", 1); //value should be increased, instead of replaced
 		transaction.IncreaseTransactionItem("CLO/1", 5);
@@ -28,6 +52,11 @@ public class TransactionTest {
 		assertFalse(5 == item1.get(0).GetProductQuantity());
 	}
 	
+	/**
+	 * Edit existing productID's quantity
+	 * expected result: quantity edited properly
+	 * @throws CustomException
+	 */
 	@Test	
 	public void EditItem() throws CustomException{	//edit need to replace the existing value
 		transaction.AddTransactionItem("CLO/1", 1, 21.45);
@@ -40,6 +69,10 @@ public class TransactionTest {
 		assertTrue(5==item1.get(0).GetProductQuantity());		
 	}
 	
+	/**
+	 * Add 2 different items into the list, pop them and make comparison
+	 * expected result: 2 different productID
+	 */
 	@Test
 	public void TestDifferentItemsAdded(){
 		transaction.AddTransactionItem("CLO/1", 1, 21.45);
@@ -48,12 +81,22 @@ public class TransactionTest {
 		assertFalse(item1.get(0).GetProductID().equals(item1.get(1).GetProductID()));
 	}
 	
+	/**
+	 * Attempt to remove non-existing productID
+	 * throw the non-existing productID exception
+	 * @throws CustomException
+	 */
 	@Test(expected=CustomException.class)
 	public void TestRemoveNonExistence() throws CustomException{ //remove item that are not existing
 		transaction.AddTransactionItem("CLO/1", 1, 21.45);
 		transaction.RemoveTransactionItem("CLO/2");
 	}
 	
+	/**
+	 * Attempt to edit non-existing productID
+	 * throw the non-existing productID exception
+	 * @throws CustomException
+	 */
 	@Test(expected=CustomException.class)
 	public void TestEditNonExistenceItems() throws CustomException{ //remove item that are not existing
 		transaction.AddTransactionItem("CLO/1", 1, 21.45);
